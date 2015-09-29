b59c92710f6e13eee2f50b98a71d3ec5d53745b4
Tim Minkov <timothyminkov@gmail.com>
2015-09-28 20:21:26 -0700

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+var App = React.createClass({
+  render: function() {
+    return (
+      <div className="beacon__background">
+      </div>
+    );
+  }
+});
+
+module.exports = App;
